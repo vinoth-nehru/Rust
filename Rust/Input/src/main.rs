b4da5b4cc72131ab27724312e576b3f40a9c6324
vinoth-nehru <vinothnehrud@gmail.com>
2024-01-11 19:55:29 +0530

    new file

--- Rust/Input/src/main.rs
+++ Rust/Input/src/main.rs
@@ -7,8 +7,6 @@ use std::fs::File;
 fn main() {
     println!("What is your name?");
     // let mut name =String::new();
-
-    
     // let greeting ="Hi, Nice to meet you.";
     // io::stdin().read_line(& mut name)
     //     .expect("Not receivded the input");
