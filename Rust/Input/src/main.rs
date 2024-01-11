b1fd1639446f46e6f4a7060b97592a6451e3332c
vinoth-nehru <vinothnehrud@gmail.com>
2024-01-11 19:55:06 +0530

    new file

--- Rust/Input/src/main.rs
+++ Rust/Input/src/main.rs
@@ -1,3 +1,16 @@
+#![allow(unused)]
+
+use std::io::*;
+use std::fs::File;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("What is your name?");
+    // let mut name =String::new();
+
+    
+    // let greeting ="Hi, Nice to meet you.";
+    // io::stdin().read_line(& mut name)
+    //     .expect("Not receivded the input");
+    // println!("Hello {}! {}",name,greeting);
 }
